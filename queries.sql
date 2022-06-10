20f488719de23de25e00ba16292a7c2a74715649
VTNoble <noble.jason91@gmail.com>
2022-06-09 21:49:59 -0400

    updated jupyter notebook with merged dataframe and sql file with table schema code

--- queries.sql
+++ queries.sql
@@ -1,19 +1,11 @@
--- Create tables for raw data to be loaded into
-CREATE TABLE customer_name (
-id INT PRIMARY KEY,
-first_name TEXT,
-last_name TEXT
+-- Create table for merged dataframe to be loaded into
+CREATE TABLE olympic_medals (
+Year INT,
+Country TEXT,
+Gold INT,
+Silver INT,
+Bronze INT,
+Total INT,
+Population FLOAT(5)
 );
 
-CREATE TABLE customer_location (
-id INT PRIMARY KEY,
-address TEXT,
-us_state TEXT
-);
-
-
--- Joins tables
-SELECT customer_name.id, customer_name.first_name, customer_name.last_name, customer_location.address, customer_location.us_state
-FROM customer_name
-JOIN customer_location
-ON customer_name.id = customer_location.id;
